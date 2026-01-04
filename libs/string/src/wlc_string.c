284961690c2ac555dd6a43cbb11d4426e2645ef5
ian <me@iany.me>
2026-01-04 18:24:36 +0800

    refactor: implement strnlen_s for compatibility

--- libs/string/src/wlc_string.c
+++ libs/string/src/wlc_string.c
@@ -2,8 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
+static size_t capped_strlen(char const *s, size_t n) {
+    size_t len = 0;
+    for (; len < n; len++) {
+        if (s[len] == '\0') {
+            break;
+        }
+    }
+    return len;
+}
+
 void string_putsn(struct string *str, char const *s, size_t n) {
-    size_t const bytes_to_copy = strnlen_s(s, n);
+    size_t const bytes_to_copy = capped_strlen(s, n);
     size_t const new_len = str->len + bytes_to_copy;
     str->contents = (char *)realloc(str->contents, new_len + 1);
     memcpy(str->contents + str->len, s, bytes_to_copy);
